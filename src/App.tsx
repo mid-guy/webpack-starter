34283e5b74a2b09da63a080c13b14cfce6634e33
admin <HieuNK@mor.com.vn>
2022-11-21 13:55:54 +0700

    complete create webpack

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import IMAGE from './React.png'
+
+const App = () => {
+  const name = '123213'
+  return <h1>React</h1>
+}
+export default App
---
+++ webpack/webpack.development.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack')
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    hot: true,
+    /**
+     * @open = --open/package.json
+     */
+    open: true
+  },
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env.name': JSON.stringify('Mode Development')
+    }),
+    new ReactRefreshWebpackPlugin()
+  ]
+}
